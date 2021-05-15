edef89dcd6f87c45857c59fb2dfab34d2abe040a
Troy <tvinsixiengmay@gmail.com>
2021-05-15 00:05:53 -0700

    Read in sample files by eluvio to Suffix Array

--- Eluvio Core Software Engineering Challenge/main.cpp
+++ Eluvio Core Software Engineering Challenge/main.cpp
@@ -1,64 +1,37 @@
-// Eluvio Core Software Engineering Challenge.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Eluvio Core Software Engineering Challenge.cpp
 //
 
 #include <iostream>
-#include <algorithm>
+#include <fstream>
+#include <string>
 #include "suffixarray.h"
 
-std::string fileName = "sample";
+std::string fileName = "sample.";
 int numFiles = 10;
-std::vector<int> testSuffixArray();
-void printVector(std::vector<int>);
 
 int main()
 {
-    //std::vector<int> test = testSuffixArray();
-    //std::vector<bool> hi(200);
-    //std::cout << hi.size() << std::endl;
-    //printVector(test);
-    SuffixArray test1;
-    test1.addString("fouree");
-    test1.addString("houraa");
-    test1.addString("bourrr");
-    test1.addString("sourbb");
-
-
-    test1.initializeSuffixArray();
-    test1.printSuffixArray();
-    std::vector<int> lcp = test1.makeLCPArray();
+    SuffixArray sa;
+    
+    for (int i = 1; i <= numFiles; i++)
+    {
+        std::ifstream fin(fileName + std::to_string(i), std::ios::binary);
+        sa.addSource(fin);
+        fin.close();
+    }
+    sa.initializeSuffixArray();
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << sa.suffixArray[i] << std::endl;
+    }
 
     std::set<int> result;
-    int max = test1.findLongestCommonStrand(2,result);
+    int max = sa.findLongestCommonStrand(2,result);
     std::cout << "Length of LCS: " << max << std::endl;
     for (auto it = result.cbegin(); it != result.cend(); it++)
-        std::cout << "File " << test1.findSuffixOriginSource(*it) << ", Offset: " << *it << std::endl;
-}
-
-void printVector(std::vector<int> v)
-{
-    for (auto i = v.begin(); i != v.end(); i++)
     {
-        std::cout << *i << ' ';
+        int origin = sa.findSuffixOriginSource(*it);
+        std::cout << "File " << origin << ", Offset: " << sa.findTrueSuffixOffset(origin, *it) << std::endl;
     }
-    std::cout << std::endl;
-}
-
-std::vector<int> testSuffixArray()
-{
-    std::string str = "cabbage";
-    std::vector<std::string> suffixes;
-    for (int i = 0 ; i < str.length(); i++)
-    {
-        suffixes.push_back(str.substr(i, str.length()));
-    }
-    suffixes.push_back(std::string(""));
-    sort(suffixes.begin(), suffixes.end());
-
-    std::vector<int> suffixArray;
-    for (auto& suffix : suffixes)
-    {
-        int offset = str.length() - suffix.length();
-        suffixArray.push_back(offset);
-    }
-    return suffixArray;
+    return 0;
 }
